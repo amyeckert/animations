b0ff8b05f50139f7e25eb0c53007955deabdd791
Amy Eckert <amyeckert@Amys-MacBook-Pro.local>
2019-10-28 10:21:09 -0400

    area map over image- clickable but tricky to style

--- webpack.common.js
+++ webpack.common.js
@@ -23,6 +23,16 @@ module.exports = {
             }
           }
         ]
+      }, 
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "imgs"
+          }
+        }
       }
     ]
   }
